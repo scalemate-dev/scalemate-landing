a4ccdad73858502b8fde28a4ee099994da37ba60
Ruslan D. <ruslan.d@letsenhance.io>
2023-11-30 13:02:40 +0100

    update twitter:image url

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -77,7 +77,7 @@ export default {
       },
       {
         name: "twitter:image",
-        content: "/og-image.png"
+        content: "https://www.scalemate.co/og-image.png"
       }
     ]
   },
