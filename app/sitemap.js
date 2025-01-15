c9b15677cff34e5b603cd57830cb3752a7ef0771
Ruslan D. <ruslan.rdo@gmail.com>
2025-01-15 21:47:12 +0100

    add sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.scalemate.co",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://www.scalemate.co/book-a-demo",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ]
+}
