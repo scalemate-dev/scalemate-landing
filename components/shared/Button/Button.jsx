f0469d1a7faccaf233d85b70a473381cf829d68e
Ruslan D. <ruslan.rdo@gmail.com>
2025-02-02 22:39:57 +0100

    Add 404 Not Found page and related components

--- components/shared/Button/Button.jsx
+++ components/shared/Button/Button.jsx
@@ -1,9 +1,9 @@
-"use client";
+"use client"
 
-import cn from "classnames";
-import Link from "next/link";
-import Spinner from "../Spinner/Spinner";
-import styles from "./Button.module.scss";
+import cn from "classnames"
+import Link from "next/link"
+import Spinner from "../Spinner/Spinner"
+import styles from "./Button.module.scss"
 
 const Button = ({
   color,
@@ -15,31 +15,36 @@ const Button = ({
   children,
   onClick,
   outline,
+  className,
   ...props
 }) => {
-  const className = cn(styles.button, {
-    [styles[color]]: color,
-    [styles.wide]: wide,
-    [styles.disabled]: disabled,
-    [styles.outline]: outline,
-  });
+  const classNames = cn(
+    styles.button,
+    {
+      [styles[color]]: color,
+      [styles.wide]: wide,
+      [styles.disabled]: disabled,
+      [styles.outline]: outline,
+    },
+    className,
+  )
 
   const handleClick = (e) => {
-    if (disabled) return;
-    onClick?.(e);
-  };
+    if (disabled) return
+    onClick?.(e)
+  }
 
   if (href) {
     return (
-      <Link href={href} className={className}>
+      <Link href={href} className={classNames}>
         {children}
       </Link>
-    );
+    )
   }
 
   return (
     <button
-      className={className}
+      className={classNames}
       disabled={disabled}
       onClick={handleClick}
       type={submit ? "submit" : "button"}
@@ -48,7 +53,7 @@ const Button = ({
       {loading && <Spinner />}
       {children}
     </button>
-  );
-};
+  )
+}
 
-export default Button;
+export default Button
