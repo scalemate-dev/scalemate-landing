864645b048fa0f8b9fddd7747ee6916a2905ae55
Ruslan Dzhafarov <chunkey95@gmail.com>
2026-02-26 21:41:42 +0100

    Feat/use case scale faster (#25)
    * feat: add visuals and data structure for scaling ad campaigns faster use case
    
    * feat: enhance content clarity and metrics for scaling ad campaigns faster use case
    
    * feat: update punctuation for improved readability in scaling ad campaigns faster use case
    
    * feat: add new URL for scaling ad campaigns faster use case and improve content clarity
    
    * feat: update button texts and improve secondary CTA links for clarity and consistency
    
    * feat: update button texts and layout for improved user engagement in WaitList component
    
    ---------
    
    Co-authored-by: nataliia.bondar <bondarnnt@gmail.com>

--- app/use-cases/_data/index.js
+++ app/use-cases/_data/index.js
@@ -1,9 +1,11 @@
 import { data as bulkAdLaunch } from "./bulk-ad-launch"
 import { data as automatedCreativeUploadMeta } from "./automated-creative-upload-meta"
+import { data as scaleAdCampaignsFaster } from "./scale-ad-campaigns-faster"
 
 const useCases = {
   "bulk-ad-launch": bulkAdLaunch,
   "automated-creative-upload-meta": automatedCreativeUploadMeta,
+  "scale-ad-campaigns-faster": scaleAdCampaignsFaster,
 }
 
 export function getUseCaseData(slug) {
